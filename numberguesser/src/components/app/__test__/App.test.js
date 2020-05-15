a17807198e0991c69401a9c4d8a711fadc14f60e
JashanB <jashan_brar50@hotmail.com>
2020-05-15 15:35:31 -0400

    completed integration testing

--- numberguesser/src/components/app/__test__/App.test.js
+++ numberguesser/src/components/app/__test__/App.test.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from 'react-dom';
 import App from './../App';
+import Result from '../../result';
 import { render, fireEvent, cleanup, waitForDomChange, getByText } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import renderer from 'react-test-renderer';
@@ -48,4 +49,18 @@ it("has negative response when range and input don't match", async () => {
   const checkButton = getByTestId('check-button');
   fireEvent.click(checkButton);
   getByText('Nooo wrong this time. Answer was 0');
+}); 
+
+
+it("resets when try again is clicked", async () => {
+  const {getByTestId, debug, queryByTestId } = render(<App></App>);
+  const input = getByTestId('number-input');
+  fireEvent.change(input, {target: {value: '-1'}});
+  const setter = getByTestId('number-setter-form');
+  fireEvent.change(setter, {target: {value: '0'}});
+  const checkButton = getByTestId('check-button');
+  fireEvent.click(checkButton);
+  const resetButton = getByTestId('try-again-button');
+  fireEvent.click(resetButton);
+  expect(queryByTestId('result')).toBeNull();
 }); 
